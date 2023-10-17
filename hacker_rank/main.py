1bf82a3a1f186a203fa51b067caec5089e545d15
Olim2508 <rahmatovolim3@gmail.com>
2023-10-17 18:30:05 +0500

    hackerrank bon appetit

--- hacker_rank/main.py
+++ hacker_rank/main.py
@@ -1,21 +1,29 @@
 
 
 def countingValleys(steps, path):
-    is_valley = False
     altitude, valley_count = 0, 0
     for s in path:
         if s == "D":
             altitude -= 1
-            if altitude < 0 and not is_valley:
-                valley_count += 1
-                is_valley = True
         else:
             altitude += 1
-            if altitude == 0 and is_valley:
-                is_valley = False
+            if altitude == 0:
+                valley_count += 1
     return valley_count
 
 
 test1 = countingValleys(8, "UDDDUDUU")
 assert test1 == 1
+
+
+def bonAppetit(bill, k, b):
+    diff = (sum(bill) - bill[k]) / 2
+    if diff != b:
+        return int(b - diff)
+    return "Bon Appetit"
+
+
+test2 = bonAppetit([3, 10, 2, 9], 1, 12)
+print("test2", test2)
+assert test2 == 5
 
